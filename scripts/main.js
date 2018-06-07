681122c6e30b33048cc5423f7275683ae6c8fd3a
Alessandro Bertozzi <32493080+MegaITA@users.noreply.github.com>
2018-06-07 21:58:13 +0200

    Disable scroll animation on mobile devices

--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,27 @@
-$(function() {
-    $.scrollify({
-        section : ".fullwidth",
-        features: ".features",
-        easing : "easeOutExpo",
-        scrollbars : true,
-        scrollSpeed : 1100,
-        standardScrollElements: ".scroll",
-        interstitialSection : ".foot",
-        before: function(section) {
-            console.log(section);
-            if(section >= 1){
-                $(".scrollTop-wrapper").css('display', 'block');
-            } else {
-                $(".scrollTop-wrapper").css('display', 'none');
+if(!(/Android|webOS|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile/i.test(navigator.userAgent) )) {
+    $(function() {
+        $.scrollify({
+            section : ".fullwidth",
+            features: ".features",
+            easing : "easeOutExpo",
+            scrollbars : true,
+            scrollSpeed : 1100,
+            standardScrollElements: ".scroll",
+            interstitialSection : ".foot",
+            before: function(section) {
+                console.log(section);
+                if(section >= 1){
+                    $(".scrollTop-wrapper").css('display', 'block');
+                } else {
+                    $(".scrollTop-wrapper").css('display', 'none');
+                }
             }
-        }
+        });
     });
-});
-
+}
 $(".scroll").on("click", function(){
     $.scrollify.move($(this).attr("href"));
 });
-
-// $(".download-btn-filled").on("click", function(){
-//     alert("Happy downloading by Chorus :)");
-// });
 
 var init = {
     method: 'GET',
@@ -32,8 +29,8 @@ var init = {
 var jarLink, exeLink, version;
 var req = new Request('https://api.github.com/repos/iAmGio/chorus/releases/latest', init);
 fetch(req, init).then(function(response){ return response.json(); }).then(function(data){
-    jarLink = data.assets[1].browser_download_url;
-    exeLink = data.assets[0].browser_download_url;
+    jarLink = data.assets[0].browser_download_url;
+    exeLink = data.assets[1].browser_download_url;
     version = data.tag_name;
     dateRaw = data.assets[0].updated_at.substring(0, data.assets[0].updated_at.length - 10);
     date = dateRaw.split("-");
